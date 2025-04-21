c8fe5557a2ca8db9376069cfe3e1951c9d846207
Muldeczek <mlodygesler@gmail.com>
2025-04-21 09:01:54 +0200

    Next commit

--- src/main/java/pl/muldek/recipesAPI/Main.java
+++ src/main/java/pl/muldek/recipesAPI/Main.java
@@ -1,18 +1,12 @@
 package pl.muldek.recipesAPI;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.muldek.recipesAPI.listeners.AnvilRecipeListener;
 import pl.muldek.recipesAPI.listeners.BrewingRecipeListener;
 import pl.muldek.recipesAPI.listeners.GrindstoneRecipeListener;
-import pl.muldek.recipesAPI.recipes.BrewingRecipe;
-
-import java.util.Arrays;
-import java.util.List;
 
 public final class Main extends JavaPlugin {
     
@@ -21,25 +15,6 @@ public final class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new AnvilRecipeListener(),this);
         getServer().getPluginManager().registerEvents(new GrindstoneRecipeListener(),this);
         getServer().getPluginManager().registerEvents(new BrewingRecipeListener(),this);
-        
-        
-        List<ItemStack> items = Arrays.asList(new ItemStack(Material.LEATHER_HELMET),
-                new ItemStack(Material.LEATHER_CHESTPLATE),
-                new ItemStack(Material.LEATHER_LEGGINGS),
-                new ItemStack(Material.LEATHER_BOOTS));
-
-        BrewingRecipe recipe = new BrewingRecipe(new ItemStack(Material.BLACK_DYE),false,
-                items,false,11,400);
-        recipe.setFunction(pair -> {
-            ItemStack item = pair.item();
-            if (item.getItemMeta() instanceof LeatherArmorMeta meta) {
-                meta.setColor(Color.BLACK);
-                item.setItemMeta(meta);
-            }
-            return item;
-        });
-        RecipesManager.addBrewingRecipe(recipe);
-        RecipesManager.addBrewingFuel(new ItemStack(Material.COAL),false,10);
         
         RecipesManager.addBrewingFuel(new ItemStack(Material.BLAZE_POWDER),false,20);
         
